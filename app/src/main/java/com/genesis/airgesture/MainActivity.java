48fe4b02b23c169377c13b89bcc748a36b73a68f
Sylvain Laure <slaure.extern@groupeseb.com>
2016-11-22 10:47:34 +0100

    activation camera

--- app/src/main/java/com/genesis/airgesture/MainActivity.java
+++ app/src/main/java/com/genesis/airgesture/MainActivity.java
@@ -1,14 +1,25 @@
 package com.genesis.airgesture;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Toast;
 
@@ -47,7 +58,7 @@ public class MainActivity extends Activity {
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        //mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
@@ -55,7 +66,7 @@ public class MainActivity extends Activity {
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         cameraPreview = (SurfaceView) findViewById(R.id.camera_preview);
-
+        setupCamera();
         mHandler = new Handler();
 
         AsyncTask<Void, Void, ArrayList<PhotoPost>> task = new AsyncTask<Void, Void, ArrayList<PhotoPost>>() {
@@ -99,6 +110,124 @@ public class MainActivity extends Activity {
             mHandler.postDelayed(this, 100);
         }
     };
+
+
+    public void setupCamera() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    0);
+
+
+            return;
+        }
+        cameraPreview.getHolder().addCallback(surfaceHolderCallback);
+        Camera camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+        Camera.Parameters camParams = camera.getParameters();
+
+        // Find a preview size that is at least the size of our IMAGE_SIZE
+        Camera.Size previewSize = camParams.getSupportedPreviewSizes().get(0);
+        for (Camera.Size size : camParams.getSupportedPreviewSizes()) {
+            if (size.width >= IMAGE_SIZE && size.height >= IMAGE_SIZE) {
+                previewSize = size;
+                break;
+            }
+        }
+        camParams.setPreviewSize(previewSize.width, previewSize.height);
+
+        // Try to find the closest picture size to match the preview size.
+        Camera.Size pictureSize = camParams.getSupportedPictureSizes().get(0);
+        for (Camera.Size size : camParams.getSupportedPictureSizes()) {
+            if (size.width == previewSize.width && size.height == previewSize.height) {
+                pictureSize = size;
+                break;
+            }
+        }
+        camParams.setPictureSize(pictureSize.width, pictureSize.height);
+        try {
+            camera.setPreviewDisplay(cameraPreview.getHolder());
+            camera.startPreview();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "La permission camera n'est pas accordée", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else {
+            setupCamera();
+        }
+    }
+
+    private SurfaceHolder.Callback surfaceHolderCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                camera.setPreviewDisplay(cameraPreview.getHolder());
+                camera.startPreview();
+            } catch (IOException e) {
+                Log.e("CAMERA SOURCE", e.getMessage());
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            if (camera != null) {
+                camera.stopPreview();
+            }
+        }
+    };
+
+
+    private Bitmap processImage(byte[] data) throws IOException {
+        // Determine the width/height of the image
+        int width = camera.getParameters().getPictureSize().width;
+        int height = camera.getParameters().getPictureSize().height;
+
+        // Load the bitmap from the byte array
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length, options);
+
+        // Rotate and crop the image into a square
+        int croppedWidth = (width > height) ? height : width;
+        int croppedHeight = (width > height) ? height : width;
+
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        Bitmap cropped = Bitmap.createBitmap(bitmap, 0, 0, croppedWidth, croppedHeight, matrix, true);
+        bitmap.recycle();
+
+        // Scale down to the output size
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(cropped, IMAGE_SIZE, IMAGE_SIZE, true);
+        cropped.recycle();
+
+        return scaledBitmap;
+    }
 
 
 }
